49b1a7fe755f082e720745912f70cc0178b5e3c9
AbelSu <abelsu1993@outlook.com>
2016-09-26 14:47:15 +0800

    WINASmallCode01
    Beginning01

---
+++ pages/featured/featured.js
@@ -0,0 +1,9 @@
+//拿到全局应用程序实例
+const app = getApp()
+
+const API_URL = 'https://api.douban.com/v2/movie/top250'
+
+//创建一个页面对象用于控制页面逻辑
+Page ({
+    data
+})
---
+++ pages/profile/profile.js
@@ -0,0 +1,13 @@
+var app = getApp()
+
+Page({
+  data: {
+    title: 'About Me',
+    userInfo: {
+      nickname: 'AbelSu',
+      realname: '苏斌',
+      wechat: 'WEDN-NET',
+      avatar: '../../images/test.jpg'
+    }
+  }
+})
